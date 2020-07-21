ec69966a1d023117b1c5ef8840355d06aaa443a3
Ana-Morales <moralesanaruth@gmail.com>
2020-07-21 17:16:47 +0000

    Define print_S and conv_HEX functions

--- print_func_2.c
+++ print_func_2.c
@@ -41,3 +41,80 @@ int print_rev(va_list str)
 	}
 	return (c);
 }
+/**
+ * print_S - prints a string
+ * @str: string to be printed
+ *
+ * Return: number of characters printed.
+ */
+int print_S(va_list str)
+{
+	int c = 0;
+	char *nul = "(null)";
+	char *s =  va_arg(str, char *);
+
+	if (s == NULL || s == 0)
+	{
+		while (*nul)
+		{
+			c += _putchar(*nul);
+			nul++;
+		}
+	}
+	else
+	{
+		while (*s)
+		{
+			if ((*s > 0 && *s <= 15))
+			{
+				_putchar('\\');
+				c++;
+				_putchar('x');
+				c++;
+				_putchar('0');
+				c++;
+				c = c + conv_HEX(*s);
+			}
+			else if ((*s > 15 && *s < 32) || *s >= 127)
+			{
+				_putchar('\\');
+				c++;
+				_putchar('x');
+				c++;
+				c = c + conv_HEX(*s);
+			}
+			else
+				c += _putchar(*s);
+			s++;
+		}
+	}
+	return (c);
+}
+/**
+ * conv_HEX - Converts to HEXA
+ * @n: Char to convert
+ *
+ * Return: number of characters printed.
+ */
+int conv_HEX(char n)
+{
+	int num = n;
+	int i, j, c = 1;
+	int arr[10];
+
+	arr[0] = num % 16;
+	for (i = num / 16; i; i = i / 16)
+	{
+		arr[c] = i % 16;
+		c++;
+	}
+	for (j = c - 1; j >= 0; j--)
+	{
+		if (arr[j] > 9)
+		{
+			arr[j] = arr[j] + 7;
+		}
+		_putchar('0' + arr[j]);
+	}
+	return (c);
+}
